6cc6721aeebfad601b165efd822d493afa382865
yango3 <o3.yang@hotmail.com>
2019-08-25 20:16:37 +0800

    Udpate createlockbancortoken

--- src/lib/catchup/parse/createlockbancortoken.ts
+++ src/lib/catchup/parse/createlockbancortoken.ts
@@ -19,8 +19,8 @@ export async function parseCreateLockBancorToken(handler: Synchro, receipt: IfPa
 
     preBalances.forEach((element: any) => {
         nameLst.push({ address: element.address });
-        amountAll += parseInt(element.amount);
-        amountAll += parseInt(element.lock_amount);
+        amountAll += parseFloat(element.amount);
+        amountAll += parseFloat(element.lock_amount);
 
         addrLst.push(element.address)
     });
@@ -37,13 +37,16 @@ export async function parseCreateLockBancorToken(handler: Synchro, receipt: IfPa
     if (feedback.err) {
         return feedback
     }
-    // update caller balance
-    feedback = await handler.laUpdateAccountTable(caller, SYS_TOKEN, TOKEN_TYPE.SYS, -fee);
-    if (feedback.err) {
-        return feedback;
-    }
 
-    if (receipt.receipt.returnCode === 0) {
+
+    if (receipt.receipt.returnCode !== 0) {
+        // update caller balance
+        feedback = await handler.laUpdateAccountTable(caller, SYS_TOKEN, TOKEN_TYPE.SYS, -fee);
+        if (feedback.err) {
+            return feedback;
+        }
+    }
+    else { // === 0
         // get add token table
         let result = await handler.pStorageDb.insertTokenTable(tokenName, tokenType, caller, datetime, Buffer.from(JSON.stringify({
             factor: factor,
@@ -56,29 +59,37 @@ export async function parseCreateLockBancorToken(handler: Synchro, receipt: IfPa
             return result
         }
 
-
-        // update accounts token account table
-        for (let i = 0; i < preBalances.length; i++) {
-            let elem = preBalances[i]
-            // Here is some problem about lock token
-            result = await handler.laUpdateAccountTable(elem.address, tokenName, tokenType, parseFloat(elem.amount));
-            if (result.err) {
-                return result
-            }
-        }
-
         // put tokenname into hash table
         result = await handler.pStorageDb.updateNameToHashTable(tokenName, HASH_TYPE.TOKEN);
         if (result.err) {
             return result
         }
 
+        // Use transaction
+        let F = factor;
+        let R = receipt.tx.value;
+        let S = amountAll;
+
+        await handler.pStorageDb.execRecord('BEGIN', {})
         // Still use network method
-        result = await handler.insertBancorTokenParameters(tokenName);
-        if (result.err) {
-            return result;
+        result = await handler.pStorageDb.insertBancorTokenTable(tokenName, F, R, S);
+
+        // update accounts token account table
+        for (let i = 0; i < preBalances.length; i++) {
+            let elem = preBalances[i]
+            // Here is some problem about lock token
+            result = await handler.laWriteAccountTable(elem.address, tokenName, tokenType, parseFloat(elem.amount));
+            if (result.err) {
+                return result
+            }
         }
 
+        let hret = await handler.pStorageDb.execRecord('COMMIT', {})
+
+        if (hret.err) {
+            await handler.pStorageDb.execRecord('ROLLBACK', {})
+            return { err: ErrorCode.RESULT_DB_TABLE_FAILED, data: null }
+        }
     }
 
     return { err: ErrorCode.RESULT_OK, data: null }
