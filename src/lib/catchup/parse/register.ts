227d808265da65ed38c995e7cc75cf06b819333f
yango3 <o3.yang@hotmail.com>
2019-09-02 10:44:54 +0800

    Update register, unregister

--- src/lib/catchup/parse/register.ts
+++ src/lib/catchup/parse/register.ts
@@ -2,6 +2,7 @@ import { IfParseReceiptItem, Synchro } from "../synchro";
 import { IFeedBack, ErrorCode } from "../../../core";
 import { HASH_TYPE, SYS_TOKEN, TOKEN_TYPE } from "../../storage/StorageDataBase";
 import { DEPOSIT_VALUE } from "../../storage/dbapi/scoop";
+import { queryCallerCreator, txFailHandle } from "./common";
 
 export async function parseRegister(handler: Synchro, recept: IfParseReceiptItem): Promise<IFeedBack> {
     let caller = recept.tx.caller;
@@ -9,6 +10,7 @@ export async function parseRegister(handler: Synchro, recept: IfParseReceiptItem
     let time = recept.block.timestamp;
     let fee = parseFloat(recept.tx.fee)
     let val = parseFloat(recept.tx.value);
+    let creator = recept.block.creator;
 
     handler.logger.info('\n## parseRegister()');
 
@@ -17,18 +19,17 @@ export async function parseRegister(handler: Synchro, recept: IfParseReceiptItem
         return (feedback);
     }
 
+    let result = await queryCallerCreator(handler, caller, creator);
+    if (result.err) {
+        return result;
+    }
+
+    let [valCaller, valCreator] = [result.data.valCaller, result.data.valCreator];
+
     handler.logger.info('parseRegister, updateBalances')
-    if (recept.receipt.returnCode !== 0) {
-        handler.logger.info('parseRegister, fee: ' + (-fee))
-        feedback = await handler.laUpdateAccountTable(caller, SYS_TOKEN, TOKEN_TYPE.SYS, - fee);
-        if (feedback.err) {
-            return feedback;
-        }
-    } else {
-        feedback = await handler.laUpdateAccountTable(caller, SYS_TOKEN, TOKEN_TYPE.SYS, -val - fee);
-        if (feedback.err) {
-            return feedback;
-        }
+    feedback = await txFailHandle(handler, caller, valCaller, creator, valCreator, fee);
+    if (feedback.err) {
+        return feedback;
     }
 
     handler.logger.info('\n## parseRegister() succeed');
@@ -41,6 +42,7 @@ export async function parseUnregister(handler: Synchro, recept: IfParseReceiptIt
     let time = recept.block.timestamp;
     let fee = parseFloat(recept.tx.fee)
     let val = DEPOSIT_VALUE;
+    let creator = recept.block.creator;
 
     handler.logger.info('\n## parseUnregister()');
     let feedback = await handler.pStorageDb.updateHashToTxAddressTable(hash, [caller], time);
@@ -48,20 +50,19 @@ export async function parseUnregister(handler: Synchro, recept: IfParseReceiptIt
         return (feedback);
     }
 
-    handler.logger.info('parseUnRegister, updateBalances')
-    if (recept.receipt.returnCode !== 0) {
-        handler.logger.info('parseRegister, fee: ' + (-fee))
-        feedback = await handler.laUpdateAccountTable(caller, SYS_TOKEN, TOKEN_TYPE.SYS, - fee);
-        if (feedback.err) {
-            return feedback;
-        }
-    } else {
-        handler.logger.info('parseRegister, fee + val: ' + (val - fee))
-        feedback = await handler.laUpdateAccountTable(caller, SYS_TOKEN, TOKEN_TYPE.SYS, val - fee);
-        if (feedback.err) {
-            return feedback;
-        }
+    let result = await queryCallerCreator(handler, caller, creator);
+    if (result.err) {
+        return result;
     }
+
+    let [valCaller, valCreator] = [result.data.valCaller, result.data.valCreator];
+
+    handler.logger.info('parseUnRegister, updateBalances')
+    feedback = await txFailHandle(handler, caller, valCaller, creator, valCreator, fee);
+    if (feedback.err) {
+        return feedback;
+    }
+    handler.logger.info('parseUnRegister, fee: ' + (-fee))
 
     handler.logger.info('\n## parseUnregister() succeed');
     return { err: ErrorCode.RESULT_OK, data: null }
